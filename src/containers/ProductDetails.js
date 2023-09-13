87586f8b45938820c6a45af0c0da6c69c1b957eb
AureaFerreira <aureaferreiraf11@gmail.com>
2023-09-13 17:38:07 -0300

    first commit

---
+++ src/containers/ProductDetails.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { selectedProduct, removeSelectedProduct } from "../redux/actions/productsActions";
+import { Button } from 'antd';
+
+const ProductDetails = () => {
+  const { productId } = useParams();
+  let product = useSelector((state) => state.product);
+  const { image, title, price, category, description } = product;
+  const dispatch = useDispatch();
+
+  const fetchProductDetail = (id) => {
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((response) => {
+        dispatch(selectedProduct(response.data));
+      });
+  };
+
+  useEffect(() => {
+    if (productId && productId !== "") {
+      fetchProductDetail(productId);
+    }
+    return () => {
+      dispatch(removeSelectedProduct());
+    };
+  }, [productId]);
+
+  return (
+    <div className="ui grid container">
+      <div className="ui placeholder segment">
+        <div className="ui two column stackable center aligned grid">
+          <div className="ui vertical divider"></div>
+          <div className="middle aligned row">
+            <div className="column lp">
+              <img className="ui fluid image" src={image} alt={title} />
+            </div>
+            <div className="column rp">
+              <h1>{title}</h1>
+              <h2>
+                <Button type="primary">R${price}</Button>
+              </h2>
+              <h3 className="ui brown block header">{category}</h3>
+              <p>{description}</p>
+              <Button type="primary">Comprar</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
