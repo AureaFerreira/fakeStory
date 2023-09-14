8d0bd401d4a9e9d090dc112368126f77e3db0565
AureaFerreira Nascimento <116111144+AureaFerreira@users.noreply.github.com>
2023-09-13 22:52:50 -0300

    Update Componentes.js

--- src/containers/Componentes.js
+++ src/containers/Componentes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const ProductComponent = () => {
+const tComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map((product) => {
     const { id, title, image, price, category } = product;
